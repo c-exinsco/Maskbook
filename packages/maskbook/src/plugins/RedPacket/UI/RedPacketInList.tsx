528efc9683ebf3dec3c3448af873466e4610526b
Hancheng Zhou <z308114274@gmail.com>
2021-02-16 13:14:00 +0800

    chore: reply review

--- packages/maskbook/src/plugins/RedPacket/UI/RedPacketInList.tsx
+++ packages/maskbook/src/plugins/RedPacket/UI/RedPacketInList.tsx
@@ -141,7 +141,6 @@ export function RedPacketInList(props: RedPacketInListProps) {
     )
 }
 export interface RedPacketInHistoryListProps {
-    key: string
     data: RedPacket_InMask_Record
     onSelect: (payload: RedPacketJSONPayload) => void
     onClose: () => void
