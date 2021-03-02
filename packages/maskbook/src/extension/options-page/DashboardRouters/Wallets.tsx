44b14c2a8b3edb535c4e768df234aa60c9faf426
Hancheng Zhou <z308114274@gmail.com>
2021-03-02 15:31:06 +0800

    chore: remove unused history code

--- packages/maskbook/src/extension/options-page/DashboardRouters/Wallets.tsx
+++ packages/maskbook/src/extension/options-page/DashboardRouters/Wallets.tsx
@@ -3,15 +3,12 @@ import { Button } from '@material-ui/core'
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
-import RestoreIcon from '@material-ui/icons/Restore'
 import DashboardRouterContainer from './Container'
 import { useModal } from '../DashboardDialogs/Base'
 import {
     DashboardWalletImportDialog,
     DashboardWalletAddERC20TokenDialog,
-    DashboardWalletHistoryDialog,
     DashboardWalletErrorDialog,
-    DashboardWalletRedPacketDetailDialog,
 } from '../DashboardDialogs/Wallet'
 import { useI18N } from '../../../utils/i18n-next-ui'
 import useQueryParams from '../../../utils/hooks/useQueryParams'
@@ -85,8 +82,6 @@ export default function DashboardWalletsRouter() {
     const [walletImport, openWalletImport] = useModal(DashboardWalletImportDialog)
     const [walletError, openWalletError] = useModal(DashboardWalletErrorDialog)
     const [addToken, , openAddToken] = useModal(DashboardWalletAddERC20TokenDialog)
-    const [walletHistory, , openWalletHistory] = useModal(DashboardWalletHistoryDialog)
-    const [walletRedPacketDetail, , openWalletRedPacketDetail] = useModal(DashboardWalletRedPacketDetailDialog)
 
     const selectedWallet = useWallet()
 
@@ -122,23 +117,6 @@ export default function DashboardWalletsRouter() {
             icon: <EthereumStatusBar />,
             handler: () => undefined,
         })
-
-    if (selectedWallet)
-        floatingButtons.push({
-            icon: <RestoreIcon />,
-            handler: () => {
-                if (!selectedWallet) return
-                openWalletHistory({
-                    wallet: selectedWallet,
-                    onRedPacketClicked(payload) {
-                        openWalletRedPacketDetail({
-                            wallet: selectedWallet,
-                            payload,
-                        })
-                    },
-                })
-            },
-        })
     //#endregion
 
     return (
@@ -162,10 +140,8 @@ export default function DashboardWalletsRouter() {
                 </div>
             </ThemeProvider>
             {addToken}
-            {walletHistory}
             {walletImport}
             {walletError}
-            {walletRedPacketDetail}
         </DashboardRouterContainer>
     )
 }
